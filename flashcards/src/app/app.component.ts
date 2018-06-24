14583a089647da0979f74f50e2b8f89938ecfb47
Steve Steele <steven.harville.steele@gmail.com>
2018-06-24 10:23:43 -0500

    Move from css to scss

--- flashcards/src/app/app.component.ts
+++ flashcards/src/app/app.component.ts
@@ -8,7 +8,7 @@ let i = Math.floor(Math.random() * translations.length);
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   prompt = translations[i].english;
