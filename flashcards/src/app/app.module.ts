2e0a2fe56894004ac8228f711977743b7556d725
Steve Steele <steven.harville.steele@gmail.com>
2018-07-12 22:06:23 -0500

    Add MD icons; Remove MD buttons

--- flashcards/src/app/app.module.ts
+++ flashcards/src/app/app.module.ts
@@ -2,8 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './components/app.component';
 import { FlashcardsComponent } from './components/flashcards/flashcards.component';
@@ -21,8 +21,8 @@ import { UserOptionsComponent } from './components/user-options/user-options.com
     BrowserModule,
     BrowserAnimationsModule,
     MatCardModule,
-    MatButtonModule,
-    MatDialogModule
+    MatDialogModule,
+    MatIconModule
   ],
   entryComponents: [UserOptionsComponent],
   providers: [],
