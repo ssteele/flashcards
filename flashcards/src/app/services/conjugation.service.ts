64aac3ccdb8317fdfff694484cb7abc62eab0efd
Steve Steele <steven.harville.steele@gmail.com>
2018-08-16 21:25:55 -0500

    Create flashcard service; Move methods out of conjugation service

--- flashcards/src/app/services/conjugation.service.ts
+++ flashcards/src/app/services/conjugation.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-import { CONSTANTS } from '../data/constants';
 import { Conjugation } from '../models/conjugation';
 import { Filter } from '../models/filter';
+import { CONSTANTS } from '../data/constants';
 import { CONJUGATIONS } from '../data/conjugations';
+import { FlashcardService } from './flashcard.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,9 @@ export class ConjugationService {
   conjugations: Conjugation[];
   conjugation: Conjugation;
 
-  constructor() {}
+  constructor(
+    private flashcardService: FlashcardService,
+  ) {}
 
   private getUnique(values) {
     return values.filter((value, i, array) => {
@@ -53,53 +56,12 @@ export class ConjugationService {
     return this.getUnique(infinitives).join(', ');
   }
 
-  private filter(conjugations: Conjugation[], filters: Filter): Conjugation[] {
-    return conjugations.filter((conjugation) => {
-      let isFound = true;
-      for (const property of this.filterProperties) {
-        isFound = filters[property].some((filter) => {
-          return conjugation[property] === filter;
-        });
-        if (!isFound) {
-          break;
-        }
-      }
-      return isFound;
-    });
-  }
-
-  private shuffle(conjugations: Conjugation[]): Conjugation[] {
-    let currentIndex = conjugations.length, temporaryValue, randomIndex;
-
-    // while there remain elements to shuffle...
-    while (0 !== currentIndex) {
-
-      // pick a remaining element
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-
-      // and swap it with the current element
-      temporaryValue = conjugations[currentIndex];
-      conjugations[currentIndex] = conjugations[randomIndex];
-      conjugations[randomIndex] = temporaryValue;
-    }
-
-    return conjugations;
-  }
-
-  private reduce(conjugations: Conjugation[], maxCount: number): Conjugation[] {
-    if (maxCount < conjugations.length) {
-      conjugations = conjugations.slice(0, maxCount);
-    }
-    return conjugations;
-  }
-
   public get(maxCount: number, filters: Filter): Conjugation[] {
     let conjugations = CONJUGATIONS;
 
-    conjugations = this.filter(conjugations, filters);
-    conjugations = this.shuffle(conjugations);
-    conjugations = this.reduce(conjugations, maxCount);
+    conjugations = this.flashcardService.filter(conjugations, filters);
+    conjugations = this.flashcardService.shuffle(conjugations);
+    conjugations = this.flashcardService.reduce(conjugations, maxCount);
 
     return conjugations;
   }
---
+++ flashcards/src/app/services/flashcard.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { Conjugation } from '../models/conjugation';
+import { Filter } from '../models/filter';
+import { CONSTANTS } from '../data/constants';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FlashcardService {
+  filterProperties: string[] = CONSTANTS.FILTERS;
+
+  constructor() { }
+
+  public filter(conjugations: Conjugation[], filters: Filter): Conjugation[] {
+    return conjugations.filter((conjugation) => {
+      let isFound = true;
+      for (const property of this.filterProperties) {
+        isFound = filters[property].some((filter) => {
+          return conjugation[property] === filter;
+        });
+        if (!isFound) {
+          break;
+        }
+      }
+      return isFound;
+    });
+  }
+
+  public shuffle(conjugations: Conjugation[]): Conjugation[] {
+    let currentIndex = conjugations.length, temporaryValue, randomIndex;
+
+    // while there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // pick a remaining element
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // and swap it with the current element
+      temporaryValue = conjugations[currentIndex];
+      conjugations[currentIndex] = conjugations[randomIndex];
+      conjugations[randomIndex] = temporaryValue;
+    }
+
+    return conjugations;
+  }
+
+  public reduce(conjugations: Conjugation[], maxCount: number): Conjugation[] {
+    if (maxCount < conjugations.length) {
+      conjugations = conjugations.slice(0, maxCount);
+    }
+    return conjugations;
+  }
+}
