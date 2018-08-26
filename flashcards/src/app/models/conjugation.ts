848d871a45ba12679b817e834194b96fb0569e00
Steve Steele <steven.harville.steele@gmail.com>
2018-08-26 13:43:56 -0500

    Add and style conjugation cues
    ...most of this done in the import conjugations from excel step

--- flashcards/src/app/models/conjugation.ts
+++ flashcards/src/app/models/conjugation.ts
@@ -3,6 +3,7 @@ import { Translation } from './translation';
 export class Conjugation {
   infinitive: Translation;
   translation: Translation;
+  cue: Translation;
   level: string;
   tense: string;
   subject: string;
